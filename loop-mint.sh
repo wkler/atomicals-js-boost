2a21ad0b13f423fb69daa28e4f1956ec9f686d25
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2024-01-01 15:58:15 +0800

    Update loop-mint.sh
    add output.log

--- loop-mint.sh
+++ loop-mint.sh
@@ -3,23 +3,26 @@
 # Specify the number of loops
 numberOfLoops=10
 
+# Specify the output file
+outputFile="output.log"
+
 # Use a for loop to execute a specified number of times
 for ((i=1; i<=numberOfLoops; i++))
 do
-    echo "This is the ${i}th loop"
+    echo "This is the ${i}th loop" | tee -a $outputFile
 
-    # Run yarn command
-    yarn cli mint-dft quark --satsbyte 100
+    # Run yarn command and append output to the file
+    yarn cli mint-dft quark --satsbyte 100 >> $outputFile 2>&1
 
     # Check whether the command was executed successfully
     if [ $? -ne 0 ]; then
-        echo "Command execution failed, try to run again..."
+        echo "Command execution failed, try to run again..." | tee -a $outputFile
     else
-        echo "The command was executed successfully and is ready for the next cycle..."
+        echo "The command was executed successfully and is ready for the next cycle..." | tee -a $outputFile
     fi
 
     # Wait for some time (optional)
     sleep 1
 done
 
-echo "loop completed"
+echo "loop completed" | tee -a $outputFile
